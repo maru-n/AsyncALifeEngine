97333b4003cba9bf5fd226c29388513d04242221
marun <norihiro.maruyama@gmail.com>
2019-10-04 22:41:44 +0900

    up version => 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ALifeEngine',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=('docker')),
     install_requires=[],
     entry_points={
